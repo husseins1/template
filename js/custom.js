ef120a15f0fce11604579a778027312baa231b1b
Hussein <hussendon3@gmail.com>
2021-02-21 20:55:10 +0300

    finish dropdown

---
+++ js/custom.js
@@ -0,0 +1,60 @@
+
+// GOOGLE MAP
+var map = '';
+var center;
+
+function initialize() {
+    var mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(13.758468, 100.567481),
+      scrollwheel: false
+    };
+  
+    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+
+    google.maps.event.addDomListener(map, 'idle', function() {
+        calculateCenter();
+    });
+  
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+}
+
+function calculateCenter() {
+  center = map.getCenter();
+}
+
+function loadGoogleMap(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    document.body.appendChild(script);
+}
+
+$(function(){
+  loadGoogleMap();
+});
+
+// NIVO LIGHTBOX
+$('#portfolio a').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+// HIDE MOBILE MENU AFTER CLIKING ON A LINK
+  //  $('.navbar-collapse a').click(function(){
+  //       $(".navbar-collapse").collapse('hide');
+  //   });
+
+document.querySelectorAll(".drop").forEach(btn=>{
+  
+  btn.addEventListener("click",()=>{
+    document.querySelectorAll(".drop").forEach(
+      s=>s===btn?"" :s.classList.remove("show")
+    )
+
+      btn.classList.toggle("show");
+
+    
+  })
+})
